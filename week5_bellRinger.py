295fd7d97e4a116f3d488578ca88e852f731a8df
ricky-012 <ejherrera289@gmail.com>
2025-11-06 18:58:02 +0000

    Manipulating

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -40,10 +40,17 @@ extracted_name = print(famous_quote[83: ])
 
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
+phrase = "Python is fun. Fun is good. Good is subjective."
 # a. Extract the word 'subjective' without knowing its exact position.
+word = print(phrase.find("subjective"))
+extracted_word = print(phrase[36: ])
 # b. Extract every third word.
+every_three_words = print(phrase[ : :3])
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
-
+words = phrase.split()
+print(words)
+reversed_words = ' '.join(reversed(words))
+print(reversed_words)
 
 
 
